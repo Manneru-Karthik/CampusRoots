2b7ff21046d72a6d01832d18c9d5625a1db97ea7
Karthik M <km9100208@gmail.com>
2024-11-25 17:49:54 +0530

    home page

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,11 @@ app.use(cors());
 mongoose.connect(process.env.MONGOOSE_URL);
 
 
+app.use("/",(req,res)=>{
+    res.send("<h1>welcome</h1>");
+})
 
-app.use("/",routes,uploadroutes);
+app.use("/alumnitracking",routes,uploadroutes);
 const server=app.listen(process.env.PORT,()=> {
     console.log(`Server established at ${process.env.PORT}`)});
 
