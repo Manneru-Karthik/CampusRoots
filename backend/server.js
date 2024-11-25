fa69bb9d9b7bf8fd727eaf858c4c9f74b8b8fdc6
Karthik M <km9100208@gmail.com>
2024-11-25 18:52:06 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,6 @@ app.use(cors());
 mongoose.connect(process.env.MONGOOSE_URL);
 
 
-app.use("/",(req,res)=>{
-    res.send("<h1>welcome</h1>");
-})
 
 app.use("/alumnitracking",routes,uploadroutes);
 const server=app.listen(process.env.PORT,()=> {
